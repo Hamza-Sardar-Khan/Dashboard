f80637e4140127b4139c96131ef591ad367983c1
Hamza-Sardar-Khan <naveedabdullah800@gmail.com>
2023-12-25 15:37:58 +0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+// App.js
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Navbar from './components/Navbar';
+import Container from './components/Container';
+import SideBar from './components/SideBar';
+import Boxes from './components/Boxes';
+import Schedule from './components/Schedule';
+
+const App = () => {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!isSidebarOpen);
+    console.log(isSidebarOpen);
+  };
+
+  return (
+    <React.StrictMode>
+      <Container>
+        <Navbar toggleSidebar={toggleSidebar} />
+
+        <div className="flex flex-row mt-3">
+          <SideBar isOpen={isSidebarOpen} />
+          <div className="flex flex-col w-full h-full">
+            <Boxes />
+            <Schedule />
+          </div>
+        </div>
+      </Container>
+    </React.StrictMode>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+root.render(<App />);
+
 reportWebVitals();
